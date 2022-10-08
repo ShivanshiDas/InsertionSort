e7b7f9253d5fef549251588f1dc98d22663640cf
Shivanshi Das <shivanshi.das@student.allenisd.org>
2022-10-08 16:52:34 -0500

    Removing print statements

--- main.swift
+++ main.swift
@@ -1,7 +1,6 @@
 
 
 
-
 var words = [String]()
 
 var line = readLine()
@@ -10,11 +9,7 @@ while line != nil && line != "" && line != " " {
     line = readLine()
 }
 
-var pass = 0
-var swapTotal = 0
-var swap = 0
 
-print("Pass: \(pass), Swaps: \(swap)/\(swapTotal), Array: \(words)")
 
 for i in 1..<words.count {
 
@@ -23,19 +18,20 @@ for i in 1..<words.count {
     // all elements of array [0,i-1] that are greater than num to one position ahead of their current position
 
     var previousNum = i - 1 // index of previousNum
-    var swap = 0
+   
     while previousNum >= 0 && currentNum < words[previousNum] {
        
         words[previousNum+1] = words[previousNum] // previousNum takes place of currentNum
         previousNum -= 1
-        swap += 1
-        swapTotal += 1
+
+
     }
     words[previousNum+1] = currentNum
-    pass += 1
-    print("Pass: \(pass), Swaps: \(swap)/\(swapTotal), Array: \(words)")
+
+
     
    
 }
 
+print(words)
 
